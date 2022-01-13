616e76e8cb6d287ddbd4e9ed82a8e145277ea64f
IAM78 <91953698+IAM78@users.noreply.github.com>
2022-01-13 17:47:18 +0330

    Add files via upload

---
+++ INSERT.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (id,name,city,country)
+VALUES(1,'Ali mohammadi','Mashhad','Iran'),
+(2,'sina sohrabi','shiraz','Iran'),
+(3,'amir miri','Mashhad','Iran'),
+(4,'donald trump','Washington','United States'); 
+
+INSERT INTO products (id,name,price,count)
+VALUES(1,'bed',12000,90),
+(2,'paper',6000000,6000),
+(3,'table',37000,11),
+(4,'moz',2000,0);
